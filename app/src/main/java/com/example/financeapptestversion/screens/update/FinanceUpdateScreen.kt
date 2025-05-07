9e331f6bd000b3a16d173372a0f983ce82f3da2f
Tsandas <georgetsandekidis@gmail.com>
2025-05-07 12:35:10 +0300

    improve ui on update screen

--- app/src/main/java/com/example/financeapptestversion/screens/update/FinanceUpdateScreen.kt
+++ app/src/main/java/com/example/financeapptestversion/screens/update/FinanceUpdateScreen.kt
@@ -6,16 +6,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.AlertDialog
@@ -23,11 +29,13 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -41,13 +49,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.room.util.TableInfo
 import coil.compose.rememberAsyncImagePainter
 import com.example.financeapptestversion.R
+import com.example.financeapptestversion.components.BottomBar
 import com.example.financeapptestversion.components.FinanceAppBar
+import com.example.financeapptestversion.components.FinanceAppBarII
 import com.example.financeapptestversion.components.RoundedButton
+import com.example.financeapptestversion.components.RoundedButtonI
 import com.example.financeapptestversion.components.showToast
 import com.example.financeapptestversion.data.DataOrException
 import com.example.financeapptestversion.model.MStockItem
@@ -62,17 +76,17 @@ fun FinanceUpdateScreen(
     viewModel: StockScreenViewModel = hiltViewModel()
 ) {
 
-    Scaffold(
-        topBar = {
-            FinanceAppBar(
-                title = "Update Stock",
-                showProfile = false,
-                navController = navController,
-                icon = Icons.Default.ArrowBack
-            ) {
+    Scaffold(topBar = {
+        FinanceAppBarII(
+            title = "Update Stock",
+            navController = navController,
+            icon = Icons.Default.ArrowBack,
+            onBackArrowClicked = {
                 navController.popBackStack()
-            }
-        }) {
+            })
+    }, bottomBar = {
+        BottomBar(navController)
+    }) {
 
         val stockInfo = produceState<DataOrException<List<MStockItem>, Boolean, Exception>>(
             initialValue = DataOrException(
@@ -90,11 +104,12 @@ fun FinanceUpdateScreen(
                 .fillMaxSize()
         ) {
             Column(
-                modifier = Modifier.padding(top = 3.dp),
+                modifier = Modifier
+                    .padding(top = 3.dp)
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-
 
                 if (stockInfo.loading == true) {
                     CircularProgressIndicator()
@@ -114,12 +129,9 @@ fun FinanceUpdateScreen(
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
                             ShowBookUpdate(stockInfo = viewModel.data.value, stockItemId)
-
                             ShowUpdateFields(stock = viewModel.data.value.data?.first { mStock ->
                                 mStock.id == stockItemId
                             }, navController)
-
-
                         }
 
                     }
@@ -161,23 +173,23 @@ fun ShowUpdateFields(stock: MStockItem?, navController: NavController) {
         val stockUpdate = changedQuantity || changedPrice
 
         val context = LocalContext.current
-
         Log.d(
-            "TAG",
-            "updates stuff price: ${priceText.value}, quantity: ${quantityText.value}"
+            "FinanceUpdateScreen",
+            "ShowUpdateFields(): price: ${priceText.value}, quantity: ${quantityText.value}"
         )
         val stockToUpdate = hashMapOf(
             "price_bought" to priceText.value.toDoubleOrNull(),
             "quantity_bought" to quantityText.value.toDoubleOrNull()
         ).toMap()
-        RoundedButton(label = "Update") {
+        RoundedButtonI(label = "Update") {
             if (priceText.value.isEmpty() || quantityText.value.isEmpty()) {
-                Toast.makeText(context, "Please enter valid price and quantity", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Please enter valid price and quantity", Toast.LENGTH_SHORT)
+                    .show()
             } else {
                 if (stockUpdate) {
                     FirebaseFirestore.getInstance().collection("stocks")
-                        .document(stock?.id.toString())
-                        .update(stockToUpdate).addOnCompleteListener { task ->
+                        .document(stock?.id.toString()).update(stockToUpdate)
+                        .addOnCompleteListener { task ->
                             showToast(context, "Stock Updated Successfully!")
                             navController.navigate(AppScreens.StocksScreen.name)
                         }.addOnFailureListener {
@@ -195,7 +207,8 @@ fun ShowUpdateFields(stock: MStockItem?, navController: NavController) {
                 title = "Remove Stock",
                 text = stringResource(id = R.string.delete_message) + "\n" + stringResource(
                     id = R.string.action_delete
-                ), openDialog
+                ),
+                openDialog
             ) {
                 FirebaseFirestore.getInstance().collection("stocks").document(stock?.id.toString())
                     .delete().addOnCompleteListener {
@@ -208,7 +221,7 @@ fun ShowUpdateFields(stock: MStockItem?, navController: NavController) {
                     }
             }
         }
-        RoundedButton(label = "Delete") {
+        RoundedButtonI(label = "Delete") {
             openDialog.value = true
         }
     }
@@ -239,8 +252,7 @@ fun ShowAlertDialog(
                     onClick = { openDialog.value = false }) {
                     Text(text = "No")
                 }
-            }
-        )
+            })
     }
 }
 
@@ -266,71 +278,76 @@ fun updateFields(
 
 
     Column(
-
+        modifier = Modifier.padding(top = 24.dp),
     ) {
 
         fun tryUpdate() {
             onSearch(priceFieldValue.value.trim(), quantityFielValue.value.trim())
         }
 
-        OutlinedTextField(
-            modifier = Modifier
-                .padding(4.dp)
-                .fillMaxWidth()
-                .background(Color.White),
-            value = priceFieldValue.value,
-            onValueChange = { input ->
-                if (input.isEmpty() || input.matches(Regex("^\\d+(\\.\\d{0,2})?$"))) {
-                    priceFieldValue.value = input
-                    tryUpdate()
-                }
-            },
-            label = { Text("Enter new buy price") },
-            singleLine = true,
-            keyboardActions = KeyboardActions { tryUpdate() })
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+            elevation = CardDefaults.cardElevation(4.dp)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    "Enter new buy price", style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(Modifier.height(8.dp))
+                TextField(
+                    value = priceFieldValue.value,
+                    onValueChange = { input ->
+                        if (input.isEmpty() || input.matches(Regex("^\\d+(\\.\\d{0,2})?$"))) {
+                            priceFieldValue.value = input
+                            tryUpdate()
+                        }
+                    },
+                    label = {
+                        Text(
+                            "Enter new buy price", style = MaterialTheme.typography.titleMedium
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(5.dp),
+                    keyboardActions = KeyboardActions { tryUpdate() })
+            }
+        }
 
-        OutlinedTextField(
-            modifier = Modifier
-                .padding(4.dp)
-                .fillMaxWidth()
-                .background(Color.White),
-            value = quantityFielValue.value,
-            onValueChange = { input ->
-                if (input.isEmpty() || input.matches(Regex("^\\d+$"))) {
-                    quantityFielValue.value = input
-                    tryUpdate()
-                }
-            },
-            label = { Text("Enter new quantity") },
-            singleLine = true,
-            keyboardActions = KeyboardActions { tryUpdate() })
+        Spacer(modifier = Modifier.height(24.dp))
 
-//        InputField(
-//            modifier = Modifier
-//                .padding(4.dp)
-//                .width(150.dp)
-//                .height(25.dp)
-//                .background(Color.White),
-//            valueState = priceFieldValue,
-//            labelId = "Enter new buy price",
-//            enabled = true,
-//            onAction = KeyboardActions {
-//                tryUpdate()
-//            })
-//
-//
-//        InputField(
-//            modifier = Modifier
-//                .padding(4.dp)
-//                .width(150.dp)
-//                .height(25.dp)
-//                .background(Color.White),
-//            valueState = priceQuantityValue,
-//            labelId = "Enter new quantity",
-//            enabled = true,
-//            onAction = KeyboardActions {
-//                tryUpdate()
-//            })
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+            elevation = CardDefaults.cardElevation(4.dp)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    "Enter new quantity: ", style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(Modifier.height(8.dp))
+                TextField(
+                    value = quantityFielValue.value,
+                    onValueChange = { input ->
+                        if (input.isEmpty() || input.matches(Regex("^\\d+$"))) {
+                            quantityFielValue.value = input
+                            tryUpdate()
+                        }
+                    },
+                    label = {
+                        Text(
+                            "Enter new quantity", style = MaterialTheme.typography.titleMedium
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(5.dp),
+                    keyboardActions = KeyboardActions { tryUpdate() })
+            }
+        }
 
     }
 
@@ -341,26 +358,18 @@ fun updateFields(
 fun ShowBookUpdate(
     stockInfo: DataOrException<List<MStockItem>, Boolean, Exception>, stockItemId: String
 ) {
+    Spacer(modifier = Modifier.width(43.dp))
+    if (stockInfo.data != null) {
+        Column(
+            modifier = Modifier.padding(4.dp), verticalArrangement = Arrangement.Center
+        ) {
 
-    Row(
+            CardListItem(stock = stockInfo.data!!.first { mStock ->
+                mStock.id == stockItemId
+            }, onPressDetails = {})
 
-    ) {
-        Spacer(modifier = Modifier.width(43.dp))
-        if (stockInfo.data != null) {
-            Column(
-                modifier = Modifier.padding(4.dp), verticalArrangement = Arrangement.Center
-            ) {
-
-                CardListItem(stock = stockInfo.data!!.first { mStock ->
-                    mStock.id == stockItemId
-                }, onPressDetails = {})
-
-            }
         }
-
     }
-
-
 }
 
 @Composable
@@ -373,32 +382,45 @@ fun CardListItem(stock: MStockItem, onPressDetails: () -> Unit) {
                 RoundedCornerShape(20.dp)
             )
             .clickable {}, elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+
     ) {
 
-        Row(
-
-        ) {
-            Image(
-                painter = rememberAsyncImagePainter(model = "https://images.financialmodelingprep.com/symbol/${stock?.symbol}.png"),
-                contentDescription = "Stock Image",
+        Row() {
+            Box(
                 modifier = Modifier
-                    .height(100.dp)
-                    .width(120.dp)
-                    .padding(4.dp)
-                    .clip(RoundedCornerShape(topStart = 20.dp))
-            )
-
-            Column(
-                horizontalAlignment = Alignment.Start
+                    .size(150.dp)
+                    .background(Color(0xFFE0E0E0), RoundedCornerShape(16.dp)),
+                contentAlignment = Alignment.Center
             ) {
-                Text(text = stock.symbol)
-                Text(text = "Price bought: ${stock.priceBought}")
-                Text(text = "Quantity bought: ${stock.quantityBought}")
+                Image(
+                    painter = rememberAsyncImagePainter(
+                        model = "https://images.financialmodelingprep.com/symbol/${stock.symbol}.png"
+                    ), contentDescription = "Stock Image", modifier = Modifier.size(100.dp)
+                )
             }
-
+            Column {
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = "${stock.symbol}:  $${stock.price}",
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = "Price bought: $${stock.priceBought}",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = if (stock.priceBought!! <= stock.price) Color(0xFF2E7D32) else Color(
+                        0xFFB22222
+                    ),
+                    fontWeight = FontWeight.SemiBold
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = "Shares bought: ${stock.quantityBought}",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
         }
-
-
     }
-
 }
